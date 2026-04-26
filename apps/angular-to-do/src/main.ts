bc5461e655bcfc239fd16408ee832e8c8122144d
Fred Yang <fredyang@semanticsworks.com>
2026-04-26 17:48:28 -0400

    feat:add msw fake backend

--- apps/angular-to-do/src/main.ts
+++ apps/angular-to-do/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { worker } from './app/msw-worker';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+worker.start().then(() => {
+	console.log('MSW worker started');
+	bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+});
