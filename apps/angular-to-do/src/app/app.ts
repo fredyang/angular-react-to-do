a553b956ef70241b81a8fd25503f6c83f0d42acf
Fred Yang <fredyang@semanticsworks.com>
2026-04-26 17:49:00 -0400

    chore: remove unused files

--- apps/angular-to-do/src/app/app.ts
+++ apps/angular-to-do/src/app/app.ts
@@ -5,9 +5,14 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   selector: 'app-root',
   template: `
-    <h1 class="text-3xl font-bold underline">{{ title }}</h1>
-    `,
-  styleUrl: './app.scss',
+    <main
+      class="min-h-screen bg-gradient-to-br from-blue-50 to-blue-200 flex flex-col items-center justify-center"
+    >
+      <div class="max-w-xl mx-auto mt-12 p-6 bg-white rounded shadow-lg">
+        <h2 class="text-3xl font-bold mb-6 text-blue-700">To-Do List</h2>
+      </div>
+    </main>
+  `,
 })
 export class App {
   protected title = 'angular-to-do';
