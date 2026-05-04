ec02c421bdb5651bc3f265b692aa9a7752628ac0
Fred Yang <fredyang@semanticsworks.com>
2026-05-04 14:20:05 -0400

    chore: add build script

--- dist.sh
+++ dist.sh
@@ -1,4 +1,4 @@
 nx run-many -t build -p angular-to-do react-to-do --skip-nx-cache
-mv dist/* ..
+mv dist/* .
 git checkout dist
 git add -A && git commit -m "dist for $(date '+%B %-d, %Y %H:%M:%S')"
