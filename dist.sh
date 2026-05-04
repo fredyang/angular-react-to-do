c524bb026b5c7441fcfa45b5291be98d405d5059
Fred Yang <fredyang@semanticsworks.com>
2026-05-04 14:58:08 -0400

    chore: update build script

--- dist.sh
+++ dist.sh
@@ -1,5 +1,5 @@
 NX_VITE_BASE_URL=/angular-react-to-do/react-to-do/ pnpm nx build react-to-do --skip-nx-cache
 pnpm nx build angular-to-do --base-href=/angular-react-to-do/angular-to-do/ --skip-nx-cache
-mv dist/* .
 git checkout dist
+mv dist/* .
 git add -A && git commit -m "dist for $(date '+%B %-d, %Y %H:%M:%S')"
